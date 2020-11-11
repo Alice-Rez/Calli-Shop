00b5d9539a1f78af6c3a2428d112136f04c0afc1
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-11 15:56:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-import StyledDiv from "./styledComponents/StyledDiv";
-import StyledHeader from "./styledComponents/StyledHeader";
-
 function App() {
-  return (
-    <StyledDiv>
-      <StyledHeader>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </StyledHeader>
-    </StyledDiv>
-  );
+  return <div>Test</div>;
 }
 
 export default App;
