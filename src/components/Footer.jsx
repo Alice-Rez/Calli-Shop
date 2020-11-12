e59c3f21260842ef50a1074190288f9fefaf33e3
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-12 22:48:42 +0100

    added footer as component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import StyledFooter from "../styledComponents/StyledFooter";
+
+export default function Footer() {
+  return <StyledFooter>footer</StyledFooter>;
+}
---
+++ src/styledComponents/StyledFooter.jsx
@@ -0,0 +1,8 @@
+const { default: styled } = require("styled-components");
+
+const StyledFooter = styled.footer`
+  grid-area: footer;
+  text-align: center;
+`;
+
+export default StyledFooter;
