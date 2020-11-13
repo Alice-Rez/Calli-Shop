6a4565942fa9c1c23f9333ec8480bd4367f63e7f
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-13 20:30:08 +0100

    reducer with initial state created

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -66,3 +66,12 @@ const initialState = {
     },
   ],
 };
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
