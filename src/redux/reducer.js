19b2aded732f8b4cbc84cc5ce5769ef21596a2f2
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-13 20:27:54 +0100

    initial state defined

---
+++ src/redux/reducer.js
@@ -0,0 +1,68 @@
+const initialState = {
+  products: [
+    {
+      id: 1,
+      name: "'Ka' - Family",
+      style: "Kaisho",
+      img: "family",
+      price: 29.99,
+      available: 15,
+    },
+    {
+      id: 2,
+      name: "'Wa' - Harmony",
+      style: "Kaisho",
+      img: "harmony",
+      price: 29.99,
+      available: 10,
+    },
+    {
+      id: 3,
+      name: "'Ai' - Love",
+      style: "Kaisho",
+      img: "love",
+      price: 34.99,
+      available: 10,
+    },
+    {
+      id: 4,
+      name: "Custom names",
+      style: "Katakana",
+      img: "names",
+      details:
+        "Whatever name you want to have written in japanese. On the image example for Alice and Richard.",
+      price: 40.99,
+      available: 30,
+    },
+    {
+      id: 5,
+      name: "Strong Will",
+      style: "Gyosho",
+      img: "strong-will",
+      details:
+        "Possible also as new-year wish - Kakizome with your own name at.",
+      price: 49.99,
+      available: 5,
+    },
+    {
+      id: 6,
+      name: "'Arigatou Gozaimasu' - Thank you",
+      style: "Hiragana/Sosho",
+      img: "arigato",
+      details:
+        "Ideal gift for your boss/teacher/every superior person you want to say thank you.",
+      price: 69.99,
+      available: 3,
+    },
+    {
+      id: 6,
+      name: "'Bushido' - The Seven Virtues of The Samurai",
+      style: "Kaisho/Gyosho",
+      img: "samurai-journey",
+      details:
+        "Collection of 6 calligraphies depicting the seven virtues of samurai.",
+      price: 109.99,
+      available: 3,
+    },
+  ],
+};
