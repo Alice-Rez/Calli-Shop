f9b5efda1c2a0179470f34e24de76f78c1b1102b
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-13 19:56:41 +0100

    styling footer correction

--- src/styledComponents/StyledFooter.jsx
+++ src/styledComponents/StyledFooter.jsx
@@ -3,7 +3,7 @@ const { default: styled } = require("styled-components");
 const StyledFooter = styled.footer`
   grid-area: footer;
   text-align: center;
-  margin: var(--space-small) 0;
+  margin: var(--space-small) var(--space-medium);
   font-size: 0.8rem;
 
   img {
