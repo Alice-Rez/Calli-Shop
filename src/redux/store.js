19c29f854d06116895585191fbc738fd10f533b4
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-14 19:02:42 +0100

    added redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-const { createStore } = require("redux");
+import logger from "redux-logger";
+
+const { createStore, applyMiddleware } = require("redux");
 const { default: reducer } = require("./reducer");
 
-const calliStore = createStore(reducer);
+const calliStore = createStore(reducer, applyMiddleware(logger));
 
 export default calliStore;
