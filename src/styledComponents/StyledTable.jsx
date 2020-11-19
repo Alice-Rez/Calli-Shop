a80997b8569db874269acd682167d89a4f2aa299
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-19 20:20:56 +0100

    table styled for most of the mobiles (not the SE)

--- src/styledComponents/StyledTable.jsx
+++ src/styledComponents/StyledTable.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import device from "./device";
 
 const StyledTable = styled.table`
   width: 100%;
@@ -12,9 +13,43 @@ const StyledTable = styled.table`
 
   tfoot tr,
   tfoot td {
-    border-top: solid 1px var(--black);
-    padding-top: var(--space-small);
+    padding-top: calc(var(--space-small) / 2);
     font-weight: 900;
+  }
+
+  @media (max-width: 768px) {
+    tr {
+      display: grid;
+      grid-template-areas: "title title delete" "price qty sum";
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+
+      td:first-of-type {
+        grid-area: title;
+      }
+
+      td:last-of-type {
+        grid-area: delete;
+      }
+    }
+
+    tfoot tr {
+      grid-template-areas: "label label" "sum sum";
+      border-top: solid 1px var(--black);
+
+      td:nth-of-type(3) {
+        justify-self: right;
+      }
+    }
+  }
+
+  @media ${device.tabletPortrait} {
+    tfoot tr,
+    tfoot td {
+      border-top: solid 1px var(--black);
+      padding-top: var(--space-small);
+      font-weight: 900;
+    }
   }
 `;
 
