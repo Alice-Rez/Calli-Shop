272369855b07aa4b26abce0fa39f84277196fbcb
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-26 00:29:51 +0100

    created skelets for order confirmation & success

---
+++ src/components/OrderConfirm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OrderConfirm() {
+  return (
+    <div>
+      Here will be summary of the order & possibility to go edit every part.....
+    </div>
+  );
+}
---
+++ src/components/OrderSuccess.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OrderSuccess() {
+  return <div>Here will be just message with successfully send order</div>;
+}
