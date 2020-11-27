17ccb50559e683c58525e9cf55c33eb24f137959
Alice-Rez <reznickova.alice@seznam.cz>
2020-11-27 22:23:25 +0100

    shipping & payment part of order works

--- src/components/OrderShipPay.jsx
+++ src/components/OrderShipPay.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addOrderInfo } from "../redux/actions";
 import StyledForm from "../styledComponents/StyledForm";
 import StyledWarning from "../styledComponents/StyledWarning";
 import OrderButtonsMain from "./OrderButtonsMain";
 
 export default function OrderPersonal(props) {
+  const delivery = useSelector((state) => state.order.delivery);
+  const payment = useSelector((state) => state.order.payment);
+  const dispatch = useDispatch();
+
   const [warning, setWarning] = useState(false);
+
   const submitOrderShipPay = (e) => {
     e.preventDefault();
-
     if (e.target.checkValidity()) {
       setWarning(false);
       props.setPage(3);
@@ -15,10 +21,87 @@ export default function OrderPersonal(props) {
       setWarning(true);
     }
   };
+
+  const getValue = (e) => {
+    dispatch(addOrderInfo(e.target.name, e.target.value));
+  };
+
   return (
     <StyledForm id="orderShipPay" onSubmit={submitOrderShipPay} noValidate>
-      <p>Here will be form with shipping & payment details</p>
-      <input type="text" required />
+      <fieldset>
+        <legend>Shipping</legend>
+        <label>
+          <input
+            type="radio"
+            name="delivery"
+            value="dhl"
+            onChange={getValue}
+            checked={delivery === "dhl" ? true : false}
+          />
+          DHL
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="delivery"
+            value="fedex"
+            onChange={getValue}
+            checked={delivery === "fedex" ? true : false}
+          />
+          FedEx
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="delivery"
+            value="person"
+            onChange={getValue}
+            checked={delivery === "person" ? true : false}
+          />
+          Pick up in shop
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>Payment</legend>
+        <label>
+          <input
+            type="radio"
+            name="payment"
+            value="paypal"
+            onChange={getValue}
+            checked={payment === "paypal" ? true : false}
+          />
+          PayPal
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="payment"
+            value="credit card"
+            onChange={getValue}
+            checked={payment === "credit card" ? true : false}
+          />
+          Credit Card
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="payment"
+            value="cash"
+            onChange={getValue}
+            checked={
+              payment === "cash" && (delivery === "person" || delivery === "")
+                ? true
+                : false
+            }
+            disabled={delivery === "dhl" || delivery === "fedex" ? true : false}
+          />
+          Cash
+          <small>
+            Possible just for picking up the delivery from shop personally
+          </small>
+        </label>
+      </fieldset>
       {warning ? <StyledWarning>Please fill all fields</StyledWarning> : null}
       <OrderButtonsMain
         textLeft={"previous"}
