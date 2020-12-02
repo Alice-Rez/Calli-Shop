2581def8c553e91a91d8f8267f496b7e59563c20
Alice-Rez <reznickova.alice@seznam.cz>
2020-12-02 21:04:37 +0100

    added links to footer navigation

--- src/components/NavigationFooter.jsx
+++ src/components/NavigationFooter.jsx
@@ -4,16 +4,16 @@ import StyledLink from "../styledComponents/StyledLink";
 export default function NavigationFooter() {
   return (
     <nav>
-      <StyledLink footer to="">
+      <StyledLink footer to="/shipping-payment">
         Shipping & Payment
       </StyledLink>
-      <StyledLink footer to="">
+      <StyledLink footer to="/terms-conditions">
         Terms & Conditions
       </StyledLink>
-      <StyledLink footer to="">
+      <StyledLink footer to="/privacy-policy">
         Privacy Policy
       </StyledLink>
-      <StyledLink footer to="">
+      <StyledLink footer to="/about">
         About us
       </StyledLink>
     </nav>
